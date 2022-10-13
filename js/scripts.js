fe01db4f23ccbd7a32794ccacd4c0556a1d00233
rianisensee <107518781+rianisensee@users.noreply.github.com>
2022-10-13 19:43:04 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,121 @@
+$( document ).ready(function() {
+
+  // Progress bar
+  let containerA = document.getElementById("circleA");
+  let circleA = new ProgressBar.Circle(containerA, {
+  });
+  let containerB = document.getElementById("circleB");
+  let circleB = new ProgressBar.Circle(containerB, {
+  step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+}
+
+  });
+
+  let containerC = document.getElementById("circleC");
+  let circleC = new ProgressBar.Circle(containerC, {
+
+  step: function(state, circle) {
+  circle.path.setAttribute('stroke', state.color);
+}
+  });
+
+  let containerD = document.getElementById("circleD");
+  let circleD = new ProgressBar.Circle(containerD, {
+
+  step: function(state, circle) {
+  circle.path.setAttribute('stroke', state.color);
+}
+
+});
+
+  // iniciando loaders quando a usuário chegar no elemento
+  let dataAreaOffset = $('#data-area').offset();
+  // stop serve para a animação não carregar mais que uma vez
+  let stop = 0;
+
+  $(window).scroll(function (e) {
+  let scroll = $(window).scrollTop();
+
+  });
+
+  // Parallax
+  // setTimeout serve para carregar primeiro as imagens
+  setTimeout(function() {
+    $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+  }, 200);
+
+  // Filtro portfólio
+
+  $('.filter-btn').on('click', function() {
+
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'dsg-btn') {
+      eachBoxes('dsg', boxes);
+    } else if(type == 'dev-btn') {
+      eachBoxes('dev', boxes);
+    } else if(type == 'seo-btn') {
+      eachBoxes('seo', boxes);
+    } else {
+      eachBoxes('all', boxes);
+    }
+
+  });
+
+  function eachBoxes(type, boxes) {
+  if(type == 'all') {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function() {
+        if(!$(this).hasClass(type)) {
+          $(this).fadeOut('slow');
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
+
+  // scroll para as seções
+
+  let navBtn = $('.nav-item');
+
+  let bannerSection = $('#mainSlider');
+  let aboutSection = $('#about-area');
+  let servicesSection = $('#services-area');
+  let teamSection = $('#team-area');
+  let portfolioSection = $('#portfolio-area');
+  let contactSection = $('#contact-area');
+
+  let scrollTo = '';
+
+  $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if(btnId == 'services-menu') {
+      scrollTo = servicesSection;
+    } else if(btnId == 'team-menu') {
+      scrollTo = teamSection;
+    } else if(btnId == 'portfolio-menu') {
+      scrollTo = portfolioSection;
+    } else if(btnId == 'contact-menu') {
+      scrollTo = contactSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+  });
+
+});

737294f2718109d15423afc1aecde35628624043
rianisensee <107518781+rianisensee@users.noreply.github.com>
2023-07-12 21:04:12 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'sobre',
+    loadChildren: () => import('./sobre/sobre.module').then( m => m.SobrePageModule)
+  },
+  {
+    path: 'procedimentos',
+    loadChildren: () => import('./procedimentos/procedimentos.module').then( m => m.ProcedimentosPageModule)
+  },
+  {
+    path: 'time',
+    loadChildren: () => import('./time/time.module').then( m => m.TimePageModule)
+  },
+  {
+    path: 'portifolio',
+    loadChildren: () => import('./portifolio/portifolio.module').then( m => m.PortifolioPageModule)
+  },
+  {
+    path: 'contato',
+    loadChildren: () => import('./contato/contato.module').then( m => m.ContatoPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}

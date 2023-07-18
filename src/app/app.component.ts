4054b664f4205a7f135e07bba6642923247e6350
rianisensee <107518781+rianisensee@users.noreply.github.com>
2023-07-17 21:15:43 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ export class AppComponent {
   public appPages = [
     { title: 'Início', url: '/folder', icon: 'home' },
     { title: 'Sobre', url: '/sobre', icon: 'information-circle' },
-    { title: 'Procedimentos', url: '/procedimentos', icon: 'chevron-expand' },
+    { title: 'Procedimentos', url: '/procedimentos', icon: 'heart' },
     { title: 'Time', url: '/time', icon: 'people-circle' },
-    { title: 'Contato', url: '/contato', icon: 'mail' },
+    { title: 'Contato', url: '/contato', icon: 'call' },
     { title: 'Login', url: '/login', icon: 'person' },
   ]
   constructor() {}
